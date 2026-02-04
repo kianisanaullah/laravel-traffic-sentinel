28086fbd5377ae5400971e6a9aba53668dfcb589
Sanaullah Kiani <kianisanaullah@gmail.com>
2026-02-04 13:36:45 +0500

    Add package routes file

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Kianisanaullah\TrafficSentinel\Http\Controllers\DashboardController;
 
-Route::middleware(config('traffic-sentinel.dashboard.middleware', ['web']))
-    ->prefix(config('traffic-sentinel.dashboard.path', 'admin/traffic-sentinel'))
-    ->group(function () {
-        Route::get('/', [DashboardController::class, 'index'])->name('traffic-sentinel.dashboard');
-    });
+Route::group([
+    'prefix' => config('traffic-sentinel.dashboard.prefix', 'admin/traffic-sentinel'),
+    'middleware' => config('traffic-sentinel.dashboard.middleware', ['web']),
+], function () {
+    Route::get('/', [\Kianisanaullah\TrafficSentinel\Http\Controllers\DashboardController::class, 'index'])
+        ->name('traffic-sentinel.dashboard');
+});
