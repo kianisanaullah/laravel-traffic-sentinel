09e40bfdd83f6e1b286de47e83f00a1ae2cb62d8
Sanaullah Kiani <kianisanaullah@gmail.com>
2026-03-21 00:08:59 +0500

    Update index.blade.php

--- resources/views/bots/index.blade.php
+++ resources/views/bots/index.blade.php
@@ -176,7 +176,7 @@
                             </div>
                         </td>
                         <td>
-                            <a href="{{ route('traffic-sentinel.bots.show')}}"
+                            <a href="{{ route('traffic-sentinel.bots.show', $bot->bot_name)}}"
                                class="btn btn-sm btn-outline-info">
                                 <i class="bi bi-eye me-1"></i> View
                             </a>
